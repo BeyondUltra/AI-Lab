35f56ea83ab805aef2f5035a09220b43da40c8d8
BeyondUltra <beyondultra24@gmail.com>
2026-05-05 09:29:27 +0530

    Update Miss-Can_DLS.c

--- Uninformed_Search/Miss-Can_DLS.c
+++ Uninformed_Search/Miss-Can_DLS.c
@@ -1,16 +1,71 @@
-void DLS(int m,int c,int boat,int d){
+#include <stdio.h>
 
-    printf("(M=%d C=%d d=%d)\n",m,c,d);
+int visited[10][10][2] = {0};
 
-    if(d==0) return;
+// -------- VALIDITY CHECK --------
+int isValid(int m, int c){
+    int mr = 3 - m; // right side missionaries
+    int cr = 3 - c; // right side cannibals
 
-    int moves[5][2]={{1,0},{0,1},{1,1}};
+    // check bounds
+    if(m < 0 || c < 0 || m > 3 || c > 3)
+        return 0;
 
-    for(int i=0;i<3;i++){
-        int nm = m - moves[i][0];
-        int nc = c - moves[i][1];
+    // missionaries eaten on left
+    if(m > 0 && m < c)
+        return 0;
 
-        if(isValid(nm,nc))
-            DLS(nm,nc,0,d-1);
+    // missionaries eaten on right
+    if(mr > 0 && mr < cr)
+        return 0;
+
+    return 1;
+}
+
+// -------- DLS --------
+void DLS(int m, int c, int boat, int d){
+
+    printf("(M=%d C=%d Boat=%d Depth=%d)\n", m, c, boat, d);
+
+    // goal: all moved to right side
+    if(m == 0 && c == 0){
+        printf("Goal reached!\n");
+        return;
     }
+
+    if(d == 0) return;
+
+    if(visited[m][c][boat]) return;
+    visited[m][c][boat] = 1;
+
+    int moves[5][2] = {
+        {1,0}, {0,1}, {1,1}, {2,0}, {0,2}
+    };
+
+    for(int i=0;i<5;i++){
+
+        int nm, nc;
+
+        if(boat == 1){ // boat on LEFT → go to RIGHT
+            nm = m - moves[i][0];
+            nc = c - moves[i][1];
+        }
+        else{ // boat on RIGHT → come back to LEFT
+            nm = m + moves[i][0];
+            nc = c + moves[i][1];
+        }
+
+        if(isValid(nm, nc)){
+            DLS(nm, nc, 1 - boat, d-1); // toggle boat
+        }
+    }
+
+    // backtrack
+    visited[m][c][boat] = 0;
+}
+
+// -------- MAIN --------
+int main(){
+    DLS(3,3,1,10); // start: all on left, boat on left
+    return 0;
 }
