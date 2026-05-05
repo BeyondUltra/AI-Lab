4a52d265a94813b3767216b995f6537d3e89f738
BeyondUltra <beyondultra24@gmail.com>
2026-05-05 08:58:12 +0530

    Update CryptArithmetic_CS.c

--- Constraint Satisfaction/CryptArithmetic_CS.c
+++ Constraint Satisfaction/CryptArithmetic_CS.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
 
-int main(){
-    int S,E,N,D,M,O,R,Y;
+int used[10] = {0};   // track used digits
+int S,E,N,D,M,O,R,Y;
 
-    for(S=1;S<=9;S++)
-    for(E=0;E<=9;E++)
-    for(N=0;N<=9;N++)
-    for(D=0;D<=9;D++)
-    for(M=1;M<=9;M++)
-    for(O=0;O<=9;O++)
-    for(R=0;R<=9;R++)
-    for(Y=0;Y<=9;Y++){
+// check full equation
+int isValid(){
+    int send = 1000*S + 100*E + 10*N + D;
+    int more = 1000*M + 100*O + 10*R + E;
+    int money=10000*M+1000*O+100*N+10*E+Y;
 
-        if(S!=E && S!=N && S!=D && S!=M &&
-           E!=N && E!=D && E!=M &&
-           N!=D && N!=M &&
-           D!=M){
+    return send + more == money;
+}
 
-            int send = 1000*S + 100*E + 10*N + D;
-            int more = 1000*M + 100*O + 10*R + E;
-            int money=10000*M+1000*O+100*N+10*E+Y;
+// recursive CSP solver
+void solve(int pos){
+    if(pos == 8){  // all variables assigned
+        if(isValid()){
+            printf("SEND=%d MORE=%d MONEY=%d\n",
+                   1000*S+100*E+10*N+D,
+                   1000*M+100*O+10*R+E,
+                   10000*M+1000*O+100*N+10*E+Y);
+        }
+        return;
+    }
 
-            if(send + more == money){
-                printf("Solution Found:\n");
-                printf("SEND=%d MORE=%d MONEY=%d\n",send,more,money);
-            }
+    for(int d=0; d<=9; d++){
+        if(!used[d]){
+
+            // assign variable based on position
+            if(pos==0){ if(d==0) continue; S=d; }
+            if(pos==1){ E=d; }
+            if(pos==2){ N=d; }
+            if(pos==3){ D=d; }
+            if(pos==4){ if(d==0) continue; M=d; }
+            if(pos==5){ O=d; }
+            if(pos==6){ R=d; }
+            if(pos==7){ Y=d; }
+
+            used[d] = 1;
+
+            solve(pos+1);   // go deeper
+
+            used[d] = 0;    // backtrack
         }
     }
+}
+
+int main(){
+    solve(0);
+    return 0;
 }
